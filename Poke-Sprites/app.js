64afa7f550900a716e5d7221fa4730ec6254c67b
salamidrus <idrussalam95gmail.com>
2020-12-15 10:32:11 +0700

    Add Label for pokemon

--- Poke-Sprites/app.js
+++ Poke-Sprites/app.js
@@ -4,9 +4,14 @@ const container = document.querySelector("#container");
 let i = 1;
 
 while (i <= 100) {
+  const pokemon = document.createElement("div");
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
   const newImg = document.createElement("img");
   newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
 
-  container.appendChild(newImg);
+  pokemon.appendChild(label);
+  pokemon.appendChild(newImg);
+  container.appendChild(pokemon);
   i++;
 }
