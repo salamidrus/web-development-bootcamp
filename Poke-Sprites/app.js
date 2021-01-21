e71867d9f7ffa8bd98990d725c8b7816df5c093d
salamidrus <idrussalam95gmail.com>
2021-01-21 11:54:15 +0700

    Add Rest Demo

--- Poke-Sprites/app.js
+++ Poke-Sprites/app.js
@@ -5,13 +5,14 @@ let i = 1;
 
 while (i <= 100) {
   const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
   const label = document.createElement("span");
   label.innerText = `#${i}`;
   const newImg = document.createElement("img");
   newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
 
-  pokemon.appendChild(label);
   pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
   container.appendChild(pokemon);
   i++;
 }
